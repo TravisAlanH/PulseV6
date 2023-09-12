12181bca953596f4e0ef5b1ace9d5f3d0943b1d5
Travis Heidelberger <70112002+TravisAlanH@users.noreply.github.com>
2023-09-12 08:57:08 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const axios = require("axios");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 
-app.get("/api", (req, res) => {
+const PORT = process.env.PORT || 3100;
+
+app.listen(PORT, () => {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
   let config = {
     method: "get",
     maxBodyLength: Infinity,
@@ -24,3 +26,23 @@ app.get("/api", (req, res) => {
       console.log(error);
     });
 });
+
+// app.get("/api", (req, res) => {
+//   let config = {
+//     method: "get",
+//     maxBodyLength: Infinity,
+//     url: "https://10.34.3.86/api/v2/dcimoperations/search/makes/hp",
+//     headers: {
+//       Authorization: "Basic YWRtaW46c3VuYmlyZA==",
+//     },
+//   };
+
+//   axios
+//     .request(config)
+//     .then((response) => {
+//       console.log(JSON.stringify(response.data));
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
