9abd16f36042ddb129d43519be2c9524771c9134
Travis Heidelberger <70112002+TravisAlanH@users.noreply.github.com>
2023-09-12 08:18:00 -0500

    Init

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Basic YWRtaW46c3VuYmlyZA==");
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch("https://10.34.3.86/api/v2/dcimoperations/search/makes/hp", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+  console.log("Server running on port 3000");
+});
