0ced17a3aef4775bd3c87f6f5561a66fdd15b179
Travis Heidelberger <70112002+TravisAlanH@users.noreply.github.com>
2023-09-12 08:51:04 -0500

    Axios Call

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,26 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const axios = require("axios");
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization", "Basic YWRtaW46c3VuYmlyZA==");
+app.use(express.json());
 
-  var requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
+app.get("/api", (req, res) => {
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "https://10.34.3.86/api/v2/dcimoperations/search/makes/hp",
+    headers: {
+      Authorization: "Basic YWRtaW46c3VuYmlyZA==",
+    },
   };
 
-  fetch("https://10.34.3.86/api/v2/dcimoperations/search/makes/hp", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
-  console.log("Server running on port 3000");
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
